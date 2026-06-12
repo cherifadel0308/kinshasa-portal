0d7a49fda77ab7c3fd81c54eb18e061972fefac0
cherifadel0308 <cherifadel0308@gmail.com>
2026-06-12 22:04:43 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        drcBlue: "#007FFF",   // Sky Blue
-        drcYellow: "#F4D03F", // Yellow Accents
-        drcRed: "#C0392B",    // Red Highlights
+        drcBlue: "#007FFF",
+        drcYellow: "#F4D03F",
+        drcRed: "#C0392B",
       },
     },
   },
