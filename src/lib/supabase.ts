168422b3369adbaa632160ea8e7eb5943a3d1652
cherifadel0308 <cherifadel0308@gmail.com>
2026-06-14 16:07:49 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// These variables pull directly from your Vercel Environment Variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = 'https://wsadnbdgqanmjhfhjyhx.supabase.co';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Initialize the client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
